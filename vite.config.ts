f7f463a1fed3ce5f8f1592ab63e6df1f40b39adc
Juanikm <136911509+Juanikm@users.noreply.github.com>
2025-08-14 19:48:02 +0000

    cambios en vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import path from 'path'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Reemplaza con el NOMBRE EXACTO del repo:
-  base: '/vertical-narrative/', 
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: '/vertical-narrative/',
 })
