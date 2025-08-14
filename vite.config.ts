66023e3ded8fe6282a8b18846932c3f2eb06b147
Juanikm <136911509+Juanikm@users.noreply.github.com>
2025-08-14 19:55:33 +0000

    hice varios cambios.

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/vertical-narrative/',
+  base: '/vertical-narrative/', // <- esto asegura que los assets se carguen correctamente en GitHub Pages
 })
